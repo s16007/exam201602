8052218548b3c4b722fc2eea6ab715fde258885a
s16007 <s16007@std.it-college.ac.jp>
2016-08-18 12:06:54 +0000

    午前の課題

--- exam/exam.py
+++ exam/exam.py
@@ -1,36 +1,49 @@
 # 'hello, {name}!'と出力してください 。
 def hello(name):
-    pass
+    print('hello, ' + name + '!')
 
 
 # sentence の文字数を出力してください
 def length(sentence):
-    pass
+    print(len(sentence))
 
 
 # sentence の2文字目から5文字目まで(5文字目は含まない)を出力してください
 def slicing2to5(sentence):
-    pass
+    print(sentence[2:5])
 
 
 # number の符号を出力してください。ただし、0は'0'と出力してください
 def number_sign(number):
-    pass
+    if number < 0:
+        print("-")
+    elif number > 0:
+        print("+")
+    else:
+        print("0")
 
 
 # number が素数なら'ok',そうでないなら'ng'と出力してください
 def prime_number(number):
-    pass
+    if number < 0:
+        print("ng")
+    if number == 2 or number == 3 or number == 5:
+        print("ok")
+    elif number % 2 == 0 or number % 3 == 0 or number % 5 == 0:
+        print("ng")
+    else:
+        print("ok")
 
 
 # 1からnumberまでの合計を出力してください
 def sum_from_1_to(number):
-    pass
+    print(sum(range(1, number + 1)))
 
 
 # numberの階乗(factorial)を出力してください
 def factorial(number):
-    pass
+    import math
+    print(math.factorial(number))
 
 
 # リストdataの各要素(整数)を3乗した結果をリスト型として返してください
@@ -60,12 +73,13 @@ def point_two_digits(a, b, c):
 
 # リストdataの内容を小さい順でソートした結果を返してください
 def list_sort(data):
-    pass
+    data.sort()
+    return data
 
 
 # 文字列の並びを逆にしたものを返してください
 def reverse_string(sentence):
-    pass
+    return sentence[::-1]
 
 
 # dateから2016年4月1日までの日数を返してください
